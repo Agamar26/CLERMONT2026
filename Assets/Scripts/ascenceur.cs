fa8d03cf10e2422d0de195143c509c08d5d64e4b
Agamar26 <lpptek26@gmail.com>
2026-09-26 00:26:31 +0200

    Update ascenceur.cs

--- Assets/Scripts/ascenceur.cs
+++ Assets/Scripts/ascenceur.cs
@@ -10,7 +10,7 @@ public class ascenceur : MonoBehaviour
     public bool changeEtage;
     public float differenceBeetwenEtage;
     public Vector2 elevatormove;
-   
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
