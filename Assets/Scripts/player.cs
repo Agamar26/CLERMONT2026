2a9b78b8e8a592c0da0029c04bfb0533da6b6757
Agamar26 <lpptek26@gmail.com>
2026-09-25 22:20:16 +0200

    sdsfdsfd

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.InputSystem;
 
 public class player : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public Vector2 move;
+    public float speed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +17,18 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(Mathf.Round(move.x) * speed, rb.linearVelocityY);
+    }
+
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
     }
 }
